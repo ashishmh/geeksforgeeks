827b37a5bd2f1417484dcb2f3fb78fe3f39d492a
Ashish Maheshwari <maheshwari.ashish1992@gmail.com>
2015-02-08 00:23:17 +0530

    slight modification to shuffle an array

--- Shuffle an array.cpp
+++ Shuffle an array.cpp
@@ -11,35 +11,37 @@ void swap(int*, int, int);
 
 void shuffleArray(int* arr, int size) {
 	srand(time(NULL));
-	for (int i = size-1; i >= 1; i--) {
-		int random = rand() % i;
-		swap(arr, random, i);
-	}
-    return;
+	for (int i = size-1; i > 0; i--)
+		swap(arr, i, rand()%i);
+	return;
 }
 
 void swap(int* arr, int a, int b) {
-	int temp = arr[b];
-	arr[b] = arr[a];
-	arr[a] = temp;
+	int temp = arr[a];
+	arr[a] = arr[b];
+	arr[b] = temp;
 	return;
 }
 
 void printArr(int* arr, int size) {
 	for (int i = 0; i < size; i++)
-		cout<<arr[i]<<endl;
+		cout<<arr[i]<<" ";
 	return;
 }
 
 int main() {
-    freopen("input.txt","r",stdin);
-
-    int size;
-    cin>>size;
-    int arr[size];
-    for (int i = 0; i < size; i++)
-    	cin>>arr[i];
-    shuffleArray(arr, size);
-    printArr(arr, size);
-    return 0;
+	freopen("input.txt","r",stdin);
+	
+	int size;
+	cin>>size;
+	int arr[size];
+	for (int i = 0; i < size; i++)
+		cin>>arr[i];
+	cout<<"original: "<<endl;
+	printArr(arr, size);
+	shuffleArray(arr, size);
+	cout<<endl<<"shuffled: "<<endl;
+	printArr(arr, size);
+	cout<<endl;
+	return 0;
 }
