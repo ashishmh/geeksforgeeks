e1899d9a249a2f2341e11bd2c25657a61616ff3f
Ashish Maheshwari <maheshwari.ashish1992@gmail.com>
2015-03-07 18:16:38 +0530

    sort array by frequency of elements

---
+++ frequency-sort.cpp
@@ -0,0 +1,74 @@
+// http://www.geeksforgeeks.org/sort-elements-by-frequency/
+
+#include <iostream>
+#include <cstdio>
+#include <vector>
+using namespace std;
+
+void frequencySort(int* arr, int n);
+bool pairCompare(const pair<int,int>& firstElem, const pair<int,int>& secondElem);
+int maxValueInArray(int* arr, int n);
+void printArr(int* arr, int size);
+
+void frequencySort(int* arr, int n) {
+	int max = maxValueInArray(arr, n);
+	int count[max+1], output[n], i;
+	for (i = 0; i < max+1; i++)
+		count[i] = 0;
+	for (i = 0; i < n; i++)
+		++count[arr[i]];
+
+	// making a vector of pair to store each distinct element and its count
+	// pair.first contains actual element and pair.second contains its count
+	vector<pair<int,int> > tempArray;
+	for (i = 0; i < max+1; i++)
+		tempArray.push_back(make_pair(i, count[i]));
+
+	// sorting the vector based on count of element in descending order
+	stable_sort(tempArray.begin(), tempArray.end(), pairCompare);
+
+	vector<pair<int, int> >::iterator itr;
+	// printing output
+	// Note: this is still not stable as giving (5,2,2,8,5,6,8,8) as input gives output 
+	// as (8,8,8,2,2,5,5,6) but it should give (8,8,8,5,5,2,2,6) for stable sort
+	cout<<endl<<"frequency sort: ";
+	for (itr = tempArray.begin(); itr < tempArray.end(); itr++)
+		for (i = itr->second; i > 0; i--) {
+			cout<<itr->first<<" ";
+			--(itr->second);
+		}
+}
+
+// function to compare elements of pair based on count in descending order
+bool pairCompare(const pair<int,int>& firstElem, const pair<int,int>& secondElem) {
+	return firstElem.second > secondElem.second;
+}
+
+int maxValueInArray(int* arr, int n) {
+	int max = arr[0];
+	for (int i = 1; i < n; i++)
+		if (arr[i] > max)
+			max = arr[i];
+	return max;
+}
+
+void printArr(int* arr, int size) {
+	for (int i = 0; i < size; i++)
+		cout<<arr[i]<<" ";
+	cout<<endl;
+}
+
+int main() {
+    freopen("input.txt","r",stdin);
+
+	int n;					// n is no of elements in input array
+	cin>>n;
+	int arr[n];
+	for (int i = 0; i < n; i++)
+		cin>>arr[i];
+	cout<<"original array: ";
+	printArr(arr, n);
+	frequencySort(arr, n);
+	cout<<endl<<endl;
+	return 0;
+}
