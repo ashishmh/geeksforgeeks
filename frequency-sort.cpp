8a7acb54195c507fe002078077341b83dfd39a6e
Ashish Maheshwari <maheshwari.ashish1992@gmail.com>
2015-03-17 00:34:14 +0530

    unstable frequency sort

--- frequency-sort.cpp
+++ frequency-sort.cpp
@@ -4,63 +4,42 @@
 #include <vector>
 using namespace std;
 
-void frequencySort(int* arr, int n);
-int maxValueInArray(int* arr, int n);
-void printArr(int* arr, int size);
+void frequencySort(int *arr, int size);
+void printArr(int *arr, int size);
 
-void frequencySort(int* arr, int n) {
-	int max = maxValueInArray(arr, n);
-	int count[max+1], output[n], i;
-	for (i = 0; i < max+1; i++)
-		count[i] = 0;
-	for (i = 0; i < n; i++)
-		++count[arr[i]];
-
+void frequencySort(int *arr, int size) {
+	int i, count[100] = {0};
+	for (i = 0; i < size; i++)
+		count[arr[i]]++;
+	
 	// making a vector of pair to store each distinct element and its count
 	// pair.first contains count and pair.second contains actual element
-	vector<pair<int,int> > tempArray;
-	for (i = 0; i < max+1; i++)
-		tempArray.push_back(make_pair(count[i], i));
+	vector<pair<int,int> > temp;
+	for (i = 0; i < 100; i++)
+		if (count[i] > 0)
+			temp.push_back(make_pair(count[i], i));
 
 	// sorting the vector based on count of element in descending order
-	stable_sort(tempArray.begin(), tempArray.end());
-	reverse(tempArray.begin(), tempArray.end());
+	sort(temp.begin(), temp.end());
+	reverse(temp.begin(), temp.end());
 
-	vector<pair<int, int> >::iterator itr;
 	// printing output
-	cout<<endl<<"frequency sort (stable): ";
-	for (itr = tempArray.begin(); itr < tempArray.end(); itr++)
-		for (i = itr->first; i > 0; i--) {
+	vector<pair<int,int> >::iterator itr;
+	for (itr = temp.begin(); itr < temp.end(); itr++)
+		for (i = itr->first; i > 0; i--)
 			cout<<itr->second<<" ";
-			--(itr->first);
-		}
 }
 
-int maxValueInArray(int* arr, int n) {
-	int max = arr[0];
-	for (int i = 1; i < n; i++)
-		if (arr[i] > max)
-			max = arr[i];
-	return max;
-}
-
-void printArr(int* arr, int size) {
+void printArr(int *arr, int size) {
 	for (int i = 0; i < size; i++)
 		cout<<arr[i]<<" ";
-	cout<<endl;
 }
 
 int main() {
-    freopen("input.txt","r",stdin);
-
-	int n;					// n is no of elements in input array
-	cin>>n;
-	int arr[n];
-	for (int i = 0; i < n; i++)
-		cin>>arr[i];
-	cout<<"original array: ";
-	printArr(arr, n);
-	frequencySort(arr, n);
-	cout<<endl<<endl;
+	int arr[] = {1,2,4,1,3,1,4,5,6,7,1,8,8,9,4};
+	printArr(arr, 15);
+	cout<<endl;
+	frequencySort(arr, 15);
+	cout<<endl;
 	return 0;
 }
