9cfa311160a0350e29ffc361768d05e8256f04e6
Ashish Maheshwari <maheshwari.ashish1992@gmail.com>
2015-06-18 17:08:45 +0530

    minor change

--- arrays/stack-using-arrays.cpp
+++ arrays/stack-using-arrays.cpp
@@ -2,82 +2,96 @@
 #include <limits>
 using namespace std;
 
-class Stack {
+class node {
 public:
-    int stackCount;
-    int stackSize;
-    int *buffer;
-    int *stackPointer;
-    Stack(int stackCount, int stackSize) {
-        this->stackCount = stackCount;
-        this->stackSize = stackSize;
-        this->buffer = new int[stackCount * stackSize + 1];
-        this->stackPointer = new int[stackCount];
+    int value;
+    int min_beneath;
+    node *next;
+    node(int value, int min_beneath) {
+        this->value = value;
+        this->min_beneath = min_beneath;
+        this->next = NULL;
     }
-    // functions
-    void push(int stackNum, int value);
-    int pop(int stackNum);
-    int peek(int stackNum);
-    bool isEmpty(int stackNum);
 };
 
-void Stack::push(int stackNum, int value) {
-    if (stackPointer[stackNum] >= stackSize) {
-        cout<<endl<<"Stack overflow"<<endl;
-        return;
-    }
-    int index = stackNum * stackSize + stackPointer[stackNum];
-    buffer[++index] = value;
-    stackPointer[stackNum]++;
+node* push(node *top, int value) {
+    if (top == NULL)
+        return new node(value, value);
+    int min = (top->min_beneath < value) ? top->min_beneath : value;
+    node *ptr = new node(value, min);
+    ptr->next = top;
+    top = ptr;
+    return top;
 }
 
-int Stack::pop(int stackNum) {
-    if (stackPointer[stackNum] < 1) {
-        cout<<endl<<"Stack underflow"<<endl;
+node* pop(node *top) {
+    if (top == NULL) {
+        cout<<"Stack underflow.."<<endl;
+        return NULL;
+    }
+    top = top->next;
+    return top;
+}
+
+int peek(node *top) {
+    if (top == NULL) {
+        cout<<"Stack empty.."<<endl;
         return INT_MIN;
     }
-    int index = stackNum * stackSize + stackPointer[stackNum];
-    int value = buffer[index];
-    buffer[index] = 0;
-    stackPointer[stackNum]--;
-    return value;
+    return top->value;
 }
 
-int Stack::peek(int stackNum) {
-    int index = stackNum * stackSize + stackPointer[stackNum];
-    return buffer[index];
+int minValue(node *top) {
+    if (top == NULL) {
+        cout<<"Stack empty.."<<endl;
+        return INT_MIN;   
+    }
+    return top->min_beneath;
 }
 
-bool Stack::isEmpty(int stackNum) {
-    return (stackPointer[stackNum] == 0);
+bool isEmpty(node *top) {
+    if (top == NULL)
+        return true;
+    return false;
 }
 
 int main()
 {
     // freopen("input.txt","r",stdin);
-    Stack *s1 = new Stack(3, 5);
-    s1->push(0, 99);
-    s1->push(0, 100);
-    s1->push(0, 101);
-    s1->push(0, 102);
-    s1->push(0, 103);
-    s1->push(0, 104);
+    node *top = push(top, 5);
+    top = push(top, 6);
+    top = push(top, 1);
+    top = push(top, 8);
 
-    cout<<"peek: "<<s1->peek(0)<<endl;    
-    s1->pop(0);
-    cout<<"peek after pop: "<<s1->peek(0)<<endl;
-    cout<<endl<<"stack pointer: "<<s1->stackPointer[0]<<endl;
-    cout<<"stack empty: ";
-    s1->isEmpty(0) ? cout<<"true" : cout<<"false";
+    int temp = peek(top);
+    cout<<"peek: "<<temp<<endl;
+    temp = minValue(top);
+    cout<<"min value: "<<temp<<endl;  
 
-    s1->pop(0);
-    s1->pop(0);
-    s1->pop(0);
-    s1->pop(0);
-    s1->pop(0);
+    top = pop(top);    
+    cout<<endl<<"top: "<<top->value<<endl;
+    
+    top = pop(top);
+    cout<<endl<<"im here"<<endl;
+    
+    // top ? cout<<"not null" : cout<<"null";
+    // cout<<endl<<top->value<<endl;
 
-    cout<<endl<<"stack empty: ";
-    s1->isEmpty(0) ? cout<<"true" : cout<<"false";
+    // cout<<endl<<"im here"<<endl;
+    // if (top == NULL)
+        // cout<<"top is null"<<endl;
+    // top = pop(top);
+    // temp = peek(top);
+    // cout<<endl<<"peek: "<<temp<<endl;
+
+    // top = pop(top);
+
+    // top = pop(top);
+    // cout<<endl;
+    // if (top)
+        // cout<<"not null";
+    // else cout<<"null";
+    // top ? cout<<"not null" : cout<<"null";
 
     cout<<endl;
     return 0;
